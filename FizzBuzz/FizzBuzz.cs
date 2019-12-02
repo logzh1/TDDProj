a7b60861cb8967d49a80fe531ee7d36b89315335
张彬强 <316455@nd.com>
2019-12-02 10:52:12 +0800

    第一版TDD， 50分钟

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,7 +1,30 @@
 ﻿
+using System.Collections.Generic;
+using System.Xml.XPath;
+
 namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        public List<string> Resolve(int i)
+        {
+            var result = new List<string>();
+            for (int value = 1; value <= i; value++)
+            {
+                var t = value.ToString();
+                if (value % 3 == 0)
+                {
+                    t = value % 5 == 0 ? "fizz buzz" : "fizz";
+                }
+                else if (value % 5 == 0)
+                {
+                    t = "buzz";
+                }
+                result.Add(t);
+            }
+            return result;
+        }
     }
+
+
 }
--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Security.Policy;
+using NUnit.Framework;
+using FizzBuzz;
 
 namespace FizzBuzzTest
 {
@@ -6,9 +9,42 @@ namespace FizzBuzzTest
     //如果这个数被3整除，打印fizz.
     //如果这个数被5整除，打印buzz. 
     //如果这个数能同时被3和5整除，打印fizz buzz.
+    //比如 n = 15, 返回一个字符串数组：
+    //[
+    //"1", "2", "fizz",
+    //"4", "buzz", "fizz",
+    //"7", "8", "fizz",
+    //"buzz", "11", "fizz",
+    //"13", "14", "fizz buzz"
+    //]
+
+
+    // 1 -> ["1"]
+    // 2 -> ["1", "2"]
+    // 3 -> ["1", "2", "fizz"]
+    // 4 -> ["1", "2", "fizz", 4]
+    // 5 -> ["1", "2", "fizz", 4, "buzz"]
+    // 15 -> ["1", "2", "fizz", 4, "buzz", "fizz", "7", "8", "fizz", "buzz", "11","buzz", "11", "fizz", "13", "14", "fizz buzz" ]
 
     [TestFixture]
     public class FizzBuzzTest
     {
+        [TestCase(1, new string[] { "1"})]
+        [TestCase(2, new string[] {"1", "2"})]
+        [TestCase(3, new string[] {"1", "2", "fizz"})]
+        [TestCase(4, new string[] {"1", "2", "fizz", "4"})]
+        [TestCase(5, new string[] {"1", "2", "fizz", "4", "buzz"})]
+        [TestCase(6, new string[] {"1", "2", "fizz", "4", "buzz", "fizz"})]
+        [TestCase(15, new string[] { "1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizz buzz" })]
+
+        public void TestInputValueExceptedResultString(int input, string[] excepted)
+        {
+            var fb = new FizzBuzz.FizzBuzz();
+            var ttt = fb.Resolve(input);
+            Assert.AreEqual(excepted, ttt.ToArray());
+        }
+
     }
+
+
 }
